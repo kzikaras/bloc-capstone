d2be2fb8de3ed04cf73d976c61b7d2a3335ba090
Kurt Zikaras <KurtZikaras@Kurts-MacBook-Pro.local>
2017-12-06 21:35:06 -0500

    Added the ability for a user to start a chat and post messages to the chat

---
+++ app/controllers/chat_rooms_controller.rb
@@ -0,0 +1,31 @@
+class ChatRoomsController < ApplicationController
+    skip_before_action :verify_authenticity_token, raise: false
+    def index
+        @chat_rooms = ChatRoom.all
+    end
+
+    def new
+        @chat_room = ChatRoom.new
+    end
+
+    def create
+        @chat_room = current_user.chat_rooms.build(chat_room_params)
+        if @chat_room.save
+            flash[:success] = 'Chat Room added!'
+            redirect_to welcome_index
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+        @message = Message.new
+    end
+
+    private
+
+    def chat_room_params
+        params.require(:chat_room).permit(:title)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :chat_rooms, only: [:new, :create, :show, :index, :post] do
+    resources :messages, only: [:new, :create, :show, :index, :post]
+  end
+  
+  
   root 'welcome#index'
 end
