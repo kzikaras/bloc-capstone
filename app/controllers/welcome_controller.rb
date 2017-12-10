c8bb53fb58a1c9cbb41e1ab4450c5c93e85a5eda
Kurt Zikaras <KurtZikaras@Kurts-MacBook-Pro.local>
2017-12-09 21:01:47 -0500

    Added a section in the standard user view to show how many instructors are signed up for HelpOut

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,8 @@ class WelcomeController < ApplicationController
   def index
     @chat_rooms = ChatRoom.all
     @user = User.all
+    @num_instructors = User.where(instructor: true).count
   end
+
+  
 end
